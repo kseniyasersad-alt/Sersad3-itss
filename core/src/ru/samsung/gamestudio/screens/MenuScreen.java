119a1a2dc55c3eb112f0ff4bf725b5277c5f5756
ItsAFeaTure-4 <saskovegor6@gmail.com>
2026-04-09 19:48:52 +0300

    8 meeting...

--- core/src/ru/samsung/gamestudio/screens/MenuScreen.java
+++ core/src/ru/samsung/gamestudio/screens/MenuScreen.java
@@ -1,6 +1,12 @@
 package ru.samsung.gamestudio.screens;
 
 import com.badlogic.gdx.ScreenAdapter;
+import ru.samsung.gamestudio.MyGdxGame;
 
 public class MenuScreen extends ScreenAdapter {
+    MyGdxGame myGdxGame;
+
+    public MenuScreen(MyGdxGame myGdxGame) {
+        this.myGdxGame = myGdxGame;
+    }
 }
